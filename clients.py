74ac926b3eda22ac07a80009cecf76f21783c1c2
jony <yhudashwartz2004@gmail.com>
2023-11-03 03:23:09 +0200

    the final

--- clients.py
+++ clients.py
@@ -1,72 +1,60 @@
 import socket
 import Trivia.graphics as graphics
 import tkinter as tk
+import select
 
 root = tk.Tk()
-my_counter = 0
-rival_counter = 0
+
+
 #open socket with server
 my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 my_socket.connect(("127.0.0.1",8822))
 print("connected")
 
 
-
-
-def updateCounters(server_sent):
-    global my_counter
-    global rival_counter
-    find_counter2_start = server_sent.find("client2 counter")
-    my_counter = int(server_sent[23:find_counter2_start])
-    rival_counter = int(server_sent[find_counter2_start + 15:])
-    
 def reciveTheFullServer_sent(x,server_sent):
     server_sent = server_sent[x:]
     return server_sent
     
     
-def close_comuunication():
-        global exit
-        exit = True
+# def close_comuunication():
+#         global exit
+#         exit = True
+        
+        
 def getAnswer(answer):
-    
-    return
-
+    my_socket.send("selected answer".encode()+str(answer).encode())
+    check = my_socket.recv(1024).decode()
+    if check == "got your answer":
+        print("my answer was sent succesfull! waiting for info")
+        ready = select.select([my_socket], [], [], 0.1)
+        while not ready[0]:
+            ready = select.select([my_socket], [], [], 0.1)
+        server_sent = my_socket.recv(1024).decode()
+        if server_sent[:4] == "info":
+            reciveTheFullServer_sent(4,server_sent)
+         ###pass the info to the gui class###
+        else:
+            print("got uncorrect answer")
+            
+def getNextQuestion():
+    my_socket.send("next question".encode())
 
 def main():
     while True:
+            ready = select.select([my_socket], [], [], 0.1)
+            while not ready[0]:
+                ready = select.select([my_socket], [], [], 0.1)
             server_sent = my_socket.recv(1024).decode()
+
             while server_sent[:8] != "question": 
                 my_socket.send("didnt got question")
                 server_sent = my_socket.recv(1024).decode()
             my_socket.send("got question".encode())
-            
-            #checking what the server sent
-            if server_sent[:8] == "question":
-                server_sent = reciveTheFullServer_sent(8,server_sent)
-            else:
-                print("404 not found")
-            # my_socket.send(str(game_graphics.score).encode())
+            server_sent = reciveTheFullServer_sent(8,server_sent)
+                ###send the question to the gui class###
                 
-            server_sent = my_socket.recv(1024).decode()
-            if server_sent[:8] == "counters":
-                updateCounters(server_sent)
-            else:
-                print("404 not found counter")
     
     print("client1 closed")
     my_socket.close()    
     
-# counter = 0   
-# def trys():
-#     global counter,my_socket
-#     counter1 = 100
-#     while counter1 > 0:
-#         server = my_socket.recv(1024).decode()
-#         counter = counter + 1
-#         print(str(counter) + server)
-#         my_socket.send("a".encode())
-#         counter1 = counter1 -1
-
-    
-# trys()
--- server.py
+++ server.py
@@ -17,26 +17,16 @@ print("secound client connected")
 
 
 
-def checkToExit(check1):
-    if check1 == "exit" or check1 == "Exit" or check1 == "EXIT":
-        return "close connection from client1"
+# def checkToExit(check1):
+#     if check1 == "exit" or check1 == "Exit" or check1 == "EXIT":
+#         return "close connection from client1"
     
-    
-    
-
-# def sendQuestionsToClient():
-#     global client_socket1,client_socket2
-#     counter = 100
-#     while counter > 0:
-#         client_socket1.send("question".encode())
-#         client_socket2.send("question".encode())
-#         check = client_socket1.recv(1024).decode()
-#         check2 = client_socket2.recv(1024).decode()
-#         while check != "a" and check2 != "a":
-#             check = client_socket1.recv(1024).decode()
-#             check2 = client_socket2.recv(1024).decode()
         
-    
+def reciveTheFullServer_sent(x,server_sent):
+    server_sent = server_sent[x:]
+    return server_sent
+
+
 def sendQuestion(question_and_ans):
     #sending to clients the question
     client_socket1.send("question".encode() + str(question_and_ans).encode())
@@ -48,61 +38,54 @@ def sendQuestion(question_and_ans):
     while check2 == "didnt got question":
         client_socket2.send("question".encode() + str(question_and_ans).encode())
     if check1 == "got question" and check2 == "got question":
-        ###update th back class that they got the questions###
+        ###update th back class that they got the questions### 
+        return
         
-def waitForAnswer():
+        
+def waitForAnswers():
     ready1 = select.select([client_socket1], [], [], 0.1)
     ready2 = select.select([client_socket2], [], [], 0.1)
     while not ready1[0]:
         ready1 = select.select([client_socket1], [], [], 0.1)
     answer1 = client_socket1.recv(1024).decode()
+    if answer1[:15] != "selected answer":
+        print("got uncorrect messege from client1")
+    else:
+        answer1 = reciveTheFullServer_sent(15,answer1)
+        client_socket1.send("got your answer".encode())
     
     while not ready2[0]:
         ready2 = select.select([client_socket2], [], [], 0.1)
     answer2 = client_socket2.recv(1024).decode() 
-    
-def main():
+    if answer2[:15] != "selected answer":
+        print("got uncorrect messege from client1")
+    else:
+        answer2 = reciveTheFullServer_sent(15,answer2)
+        client_socket2.send("got your answer".encode())
+     ###pass the answers to the back###
+     
+def infoForClients(info):
+    client_socket1.send("info".encode() + str(info).encode())
+    client_socket2.send("info".encode() + str(info).encode())
+    ready1 = select.select([client_socket1], [], [], 0.1)
+    ready2 = select.select([client_socket2], [], [], 0.1)
+    while not ready1[0]:
+        ready1 = select.select([client_socket1], [], [], 0.1)
+    answer1 = client_socket1.recv(1024).decode()
+    if answer1[:13] != "next question":
+        print("got unccorect messege from client1")
+    else:
+        while not ready2[0]:
+            ready2 = select.select([client_socket2], [], [], 0.1)
+        answer2 = client_socket2.recv(1024).decode()
+        if answer2[:13] != "next question":
+            print("got unccorect messege")
+        else:
+           ###ask the back class for next question###
+           return
+        
+
    
-    
-        
-    data = api_class.get_trivia_questions(amount=2)
-    #the sockets will ryn until the while is closed
-    while True:
-         #checking if need to exit game
-        # check1 = client_socket1.recv(1024)
-        # check2 = client_socket2.recv(1024)
-        # check_to_exit = checkToExit(check1.decode())
-        # if check_to_exit[:16] == "close connection":
-        #     if check_to_exit[-7:] == "client1":
-        #         client_socket1.send("closing: the game was closed by you:".encode())
-        #         # client_socket2.send("closing: the game was closed by client1".encode())
-        #         break
-        #     else:
-        #         client_socket1.send("closing: the game was closed by client2".encode())
-        #         # client_socket2.send("closing: the game was closed by you:".encode())
-        #         break
-        # else:
-        
-        # client_socket2.send("data".encode() + str(data).encode())
-        
-        #getting the chossen score from clients
-        client1_score = client_socket1.recv(1024).decode()
-        # client2_score = client_socket2.recv(1024).decode()
-        
-        print(client1_score + "gfcvgbhnjkhgfd")
-        client1_counter = int(client1_score)
-        # client2_counter = int(client2_score)
-            
-        #sending the counters to the clients
-        client_socket1.send("counters".encode() + "client1 counter".encode() + str(client1_counter).encode() + "client2 counter".encode() + str(client2_counter).encode())    
-        # client_socket2.send("counters".encode() + "client1 counter".encode() + str(client1_counter).encode() + "client2 counter".encode() + str(client2_counter).encode())    
-        
-        
-    print("closing connections:")    
-    client_socket1.close()
-    # client_socket2.close()
-    
-# sendQuestionsToClient()
             
         
         
