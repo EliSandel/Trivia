657ef92c3d39c67aa3040ad71d1d227b51e8fe98
Eli Sandel <eli@bestjewishguide.com>
2023-11-03 02:50:20 +0200

    f

--- backend.py
+++ backend.py
@@ -2,9 +2,34 @@ import question_data
 
 class Backend():
     def __init__(self):
+        self.turn_counter = 0
+        self.score = [0, 0]  #assuming two players
+        self.trivia_api = question_data.TriviaApi()
+        self.data = self.trivia_api.get_trivia_questions()
+        self.next_question()
         
-        pass
-    
-    
-    def start_game():
+    def check_answer(self, player, answer):
+        if answer == self.data[self.turn_counter]['correct_answer']:
+            self.correct_answer(player)
+
+    def correct_answer(self, player):
+        self.score[player] += 1
+
+    def next_question(self):
+        question = self.data[self.turn_counter]
+        self.turn_counter += 1
+        return question
+
+    def get_score(self, player):
+        return self.score[player]
+        
+        
+
+
+
+
+
+# if __name__== "__main__":
+#     print(next_question())
+#     print(turn_counter)
         
