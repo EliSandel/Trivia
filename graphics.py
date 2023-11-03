c8def73eb71560ef3d1c24281682624fce961549
Eli Sandel <eli@bestjewishguide.com>
2023-11-03 05:45:52 +0200

    f

--- graphics.py
+++ graphics.py
@@ -36,7 +36,6 @@ class GameGraphics:
         
     def next_question(self, data):
         print(data)
-        data = data[1:-1]   
         self.question_label.config(text=data['question'])
         self.buttons['a'].config(text=data['all_answers'][0], state= "enabled")
         self.buttons['b'].config(text=data['all_answers'][1], state= "enabled")
