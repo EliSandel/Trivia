8f39d2320832d6fec13a76928fb829685c226d8b
Eli Sandel <eli@bestjewishguide.com>
2023-11-05 01:42:12 +0200

    everything works. need to add game over

--- backend.py
+++ backend.py
@@ -4,19 +4,23 @@ class Backend():
     def __init__(self):
         self.turn_counter = 0
         self.score = [0, 0]  #assuming two players
+        self.amount_of_players_answered = 0
         self.trivia_api = question_data.TriviaApi()
         self.data = self.trivia_api.get_trivia_questions()
         
     def check_answer(self, player, answer):
         if answer == self.data[self.turn_counter]['correct_answer']:
             self.correct_answer(player)
+        self.amount_of_players_answered += 1
+        if self.amount_of_players_answered == 2:
+            self.amount_of_players_answered = 0
+            self.turn_counter += 1
 
     def correct_answer(self, player):
         self.score[player] += 1
 
     def next_question(self):
         question = self.data[self.turn_counter]
-        self.turn_counter += 1
         return question
 
     def get_score(self, player):
