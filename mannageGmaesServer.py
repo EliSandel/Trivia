7286fb42b59641617e92e9baa5d19fc034f843f1
jony <yhudashwartz2004@gmail.com>
2023-11-06 00:31:41 +0200

    orgenized the server and clients code

--- mannageGmaesServer.py
+++ mannageGmaesServer.py
@@ -1,9 +1,76 @@
 import server
+import socket
+import threading
 
-games = []
+class Rooms():
+    def __init__(self):
+        self.room_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.room_socket.bind(("0.0.0.0",8833))
+        self.room_socket.listen()
+        print("room socket is ready!")
+        self.roomsID = []
+        self.roomsSockets = []
+        self.room_num = 0
 
-def createNewGame():
-    games.append(server)
+
+
+    def main(self):
+        while True:
+            (client_room,client_address) = self.room_socket.accept() 
+            client_request = client_room.recv(1024).decode()
+            find_N = client_request.find("N")
+            his_mind = client_request[:find_N]
+            
+            if his_mind == "create room":
+               threading.Thread(target=self.checkForStartingGmae,args=(client_room,)).start()
+               self.createRoom(client_request,client_room,find_N)
+                
+            elif his_mind == "join room":
+                self.joinRoom(client_request,client_room,find_N)
+                
+            
+                
+                
+    def checkForStartingGmae(self,client_room):
+        while True:
+            client_request = client_room.recv(1024).decode()
+            if client_request[:10] == "start game":
+                self.startingGame(client_request)  
+            
+                 
+                
+    def startingGame(self,client_request):
+        # s = server.Server()
+        find_R = client_request.find("R")
+        number_of_room = client_request[find_R +1:]
+        the_list_of_sockets = self.roomsSockets[int(number_of_room)]
+        for x in the_list_of_sockets:
+                client_room = x
+                client_room.send("resuming".encode())
+        
+        
+    def joinRoom(self,client_request,client_room,find_N):
+        find_R = client_request.find("R")
+        room_number = client_request[find_R+1:]
+        username = client_request[find_N + 1:find_R]
+        self.roomsID[int(room_number)].append(username)
+        self.roomsSockets[int(room_number)].append(client_room)
+        client_room.send("joined succesfully".encode())
+        print("joined room")
+                
+                
+
+    def createRoom(self,client_request,client_room,find_N):
+        username = client_request[find_N + 1:]
+        self.roomsID.append([username])
+        self.roomsSockets.append([client_room])
+        client_room.send("room was created succesfully".encode() + "N".encode() + str(self.room_num).encode())
+        self.room_num += 1
+        print("room created!")
+        
+
     
-def closeGame(gameNumber):
-    games.pop(gameNumber)
+        
+if __name__ == '__main__':
+   rooms = Rooms()
+   rooms.main()
--- server.py
+++ server.py
@@ -8,108 +8,85 @@ client2_counter = 0
 backend = backend.Backend()
 api_class = question_data.TriviaApi()  
 
-server_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket1.bind(("0.0.0.0",8822))
-server_socket1.listen()
-print("socket1 is up and ready!")
-(client_socket1,client_address1) = server_socket1.accept()
-print("first client connected")
-(client_socket2,client_address2) = server_socket1.accept()
-print("secound client connected")
-
-
-
-# def checkToExit(check1):
-#     if check1 == "exit" or check1 == "Exit" or check1 == "EXIT":
-#         return "close connection from client1"
+class Server():
     
-        
-def reciveTheFullServer_sent(x,server_sent):
-    server_sent = server_sent[x:]
-    return server_sent
+    def __init__(self):
+        server_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket1.bind(("0.0.0.0",8833))
+        server_socket1.listen()
+        print("socket1 is up and ready!")
+        (self.client_socket1,self.client_address1) = server_socket1.accept()
+        print("first client connected")
+        (self.client_socket2,self.client_address2) = server_socket1.accept()
+        print("secound client connected")
+        self.question_and_ans = backend.next_question()
+
 
 
     
-def sendQuestion():
-    flag1 = 0
-    flag2 = 0
-    question_and_ans = backend.next_question()
-    #sending to clients the question
-    client_socket1.send("question".encode() + str(question_and_ans).encode())
-    client_socket2.send("question".encode() + str(question_and_ans).encode())
-    print("question sent")
-    ready1 = client_socket1.recv(1024).decode() 
-    ready2 = client_socket2.recv(1024).decode()
-    while not ready1[0] and flag1 == 0:
-        ready1 = select.select([client_socket1], [], [], 0.1)
-        check1 = client_socket1.recv(1024).decode() 
-        if check1 == "didnt got question":
-            client_socket1.send("question".encode() + str(question_and_ans).encode())
-        if check1 == "got question":
-            flag1 = 1
         
-    while not ready2[0] and flag2 != 0:
-        ready2 = select.select([client_socket1], [], [], 0.1)
-        check2 = client_socket2.recv(1024).decode() 
-        if check2 == "didnt got question":
-            client_socket2.send("question".encode() + str(question_and_ans).encode())
-        if check2 == "got question":
-            flag2 = 1
-    waitForAnswers()
-    # if check1 == "got question" and check2 == "got question":
-    #     ###you can update th back class that they got the questions### 
-    #     pass
-        
-        
-def waitForAnswers():
-    ready1 = select.select([client_socket1], [], [], 0.1)
-    ready2 = select.select([client_socket2], [], [], 0.1)
-    while not ready1[0]:
-        ready1 = select.select([client_socket1], [], [], 0.1)
-    answer1 = client_socket1.recv(1024).decode()
-    if answer1[:15] != "selected answer":
-        print("got uncorrect messege from client1")
-    else:
-        answer1 = reciveTheFullServer_sent(15,answer1)
-        client_socket1.send("got your answer".encode())
-    
-    while not ready2[0]:
-        ready2 = select.select([client_socket2], [], [], 0.1)
-    answer2 = client_socket2.recv(1024).decode() 
-    if answer2[:15] != "selected answer":
-        print("got uncorrect messege from client1")
-    else:
-        answer2 = reciveTheFullServer_sent(15,answer2)
-        client_socket2.send("got your answer".encode())
-        
-        backend.check_answer(0,answer1)
-        backend.check_answer(1,answer2)        
-        infoForClients()
-                         
-def infoForClients():
-    player1_score = backend.get_score(0)
-    player2_score = backend.get_score(1)
-    
-    client_socket1.send("info".encode() + "a".encode() + str(player1_score).encode() + "b".encode() + str(player2_score).encode())
-    client_socket2.send("info".encode() + "a".encode() + str(player2_score).encode() + "b".encode() + str(player1_score).encode())
-    
-    ready1 = select.select([client_socket1], [], [], 0.1)
-    ready2 = select.select([client_socket2], [], [], 0.1)
-    while not ready1[0]:
-        ready1 = select.select([client_socket1], [], [], 0.1)
-    answer1 = client_socket1.recv(1024).decode()
-    if answer1[:13] != "next question":
-        print("got unccorect messege from client1")
-    else:
-        while not ready2[0]:
-            ready2 = select.select([client_socket2], [], [], 0.1)
-        answer2 = client_socket2.recv(1024).decode()
-        if answer2[:13] != "next question":
-            print("got unccorect messege")
-        else:
-            sendQuestion()        
+    def main(self):
+        while True:
+            print("while")
+            check1 = self.client_socket1.recv(1024).decode() 
+            check2 = self.client_socket2.recv(1024).decode() 
+            print(check1)
+            print(check2)
+            if check1 == "didnt got question":
+                print("1")
+                self.sendQuestion() 
+            if check2 == "didnt got question":
+                print("2")       
+                self.sendQuestion()      
+            
+            if check1[:15] == "selected answer" and check2[:15] == "selected answer":  
+                print("4")
+                self.waitForAnswers(check1,check2)
+            if check1[:13] == "next question" and  check2[:13] == "next question":
+                print("5")
+                self.question_and_ans = backend.next_question()
+                self.sendQuestion()
+            
+                 
+            
+            
+    def reciveTheFullServer_sent(self,x,server_sent):
+        server_sent = server_sent[x:]
+        return server_sent
 
-   
-sendQuestion()
         
         
+    def sendQuestion(self):
+       
+        self.client_socket1.send("question".encode() + str(self.question_and_ans).encode())
+        self.client_socket2.send("question".encode() + str(self.question_and_ans).encode())
+        print("question sent")
+        
+        self.main()
+        
+            
+            
+    def waitForAnswers(self,answer1,answer2):
+       
+            answer1 = self.reciveTheFullServer_sent(15,answer1)
+           
+            answer2 = self.reciveTheFullServer_sent(15,answer2)
+            
+            backend.check_answer(0,answer1)
+            backend.check_answer(1,answer2)        
+            self.infoForClients()
+                            
+    def infoForClients(self):
+        player1_score = backend.get_score(0)
+        player2_score = backend.get_score(1)
+        
+        self.client_socket1.send("info".encode() + "a".encode() + str(player1_score).encode() + "b".encode() + str(player2_score).encode())
+        self.client_socket2.send("info".encode() + "a".encode() + str(player2_score).encode() + "b".encode() + str(player1_score).encode())
+        
+       
+        
+if __name__ == '__main__':
+    server = Server()
+    server.sendQuestion()
+            
+            
