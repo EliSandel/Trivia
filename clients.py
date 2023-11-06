cbb27e348092b7cc350b27a71d13e724b7292f41
jony <yhudashwartz2004@gmail.com>
2023-11-06 02:38:25 +0200

    changes the server for dynamic

--- clients.py
+++ clients.py
@@ -14,34 +14,38 @@ class Clients():
         self.gamegraphics = graphics.GameGraphics(self.root, self)
         
 
-    # def openClient(self):
-    #     self.my_socket_room.connect(("127.0.0.1",8833))
-    #     print("connected to rooms server")
-    #     his_mind = input("options: create room/join room?")
-    #     get_username = input("whats your name?")
-    #     if his_mind == "join room":
-    #         get_num_of_room = input("whats the number of room?")
-    #         self.my_socket_room.send(his_mind.encode() + "N".encode() +  str(get_username).encode() + "R".encode() + str(get_num_of_room).encode())
-    #         rooms_answer = self.my_socket_room.recv(1024).decode()
-    #     else:
-    #         self.my_socket_room.send(his_mind.encode() + "N".encode() +  str(get_username).encode())
-    #         rooms_answer = self.my_socket_room.recv(1024).decode()
-    #         if rooms_answer[:28] == "room was created succesfully":
-    #             find_N = rooms_answer.find("N")
-    #             my_room_number = rooms_answer[find_N+1:]
-    #             startGame = input("press enter to start geme")
-    #             self.my_socket_room.send("start game".encode() + "R".encode() + my_room_number.encode())
+    def openClient(self):
+        self.my_socket_room.connect(("127.0.0.1",8833))
+        print("connected to rooms server")
+        his_mind = input("options: create room/join room?")
+        get_username = input("whats your name?")
+        if his_mind == "join room":
+            get_num_of_room = input("whats the number of room?")
+            self.my_socket_room.send(his_mind.encode() + "N".encode() +  str(get_username).encode() + "R".encode() + str(get_num_of_room).encode())
+            rooms_answer = self.my_socket_room.recv(1024).decode()
+        else:
+            self.my_socket_room.send(his_mind.encode() + "N".encode() +  str(get_username).encode())
+            rooms_answer = self.my_socket_room.recv(1024).decode()
+            if rooms_answer[:28] == "room was created succesfully":
+                find_N = rooms_answer.find("N")
+                my_room_number = rooms_answer[find_N+1:]
+                startGame = input("press enter to start geme")
+                self.my_socket_room.send("start game".encode() + "R".encode() + my_room_number.encode())
         
         
         
         
-    #     if rooms_answer[:28] == "room was created succesfully" or rooms_answer == "joined succesfully":
-    #         print(rooms_answer)
-    #         rooms_answer = self.my_socket_room.recv(1024).decode()
-    #         print(rooms_answer)
+        if rooms_answer[:28] == "room was created succesfully" or rooms_answer == "joined succesfully":
+            print(rooms_answer)
+            rooms_answer = self.my_socket_room.recv(1024).decode()
+            print(rooms_answer)
+            rooms_answer = self.my_socket_room.recv(1024).decode()
+            print(rooms_answer)
+    
+    
             #open socket with server
-        self.my_socket.connect(("127.0.0.1",8833))
-        print("connected")
+        # self.my_socket.connect(("127.0.0.1",8833))
+        # print("connected")
 
 
     def reciveTheFullServer_sent(self,x,server_sent):            
@@ -54,11 +58,11 @@ class Clients():
     def sendInfoToGraphics(self,server_sent):
         print("got info")
         server_sent = self.reciveTheFullServer_sent(4,server_sent)
-        find_b = server_sent.find("b")
-        player1_score = server_sent[1:find_b]
-        player2_score = server_sent[find_b + 1:]
+        find_o = server_sent.find("o")
+        my_score = server_sent[1:find_o]
+        others_score = server_sent[find_o + 1:]
         print("got answer")
-        self.gamegraphics.recieve_players_score(player1_score,player2_score) 
+        self.gamegraphics.recieve_players_score(my_score,others_score) 
         
         
     def getAnswer(self,answer):
@@ -95,7 +99,7 @@ class Clients():
     
         
     def startGmae(self):
-        # self.openClient()
+        self.openClient()
         thread2 = threading.Thread(target=self.main)
         thread2.start()
         self.runGui()
--- server.py
+++ server.py
@@ -6,43 +6,56 @@ import backend
 
 class Server():
     
-    def __init__(self):
+    def __init__(self,array_of_sockets):
         self.backend = backend.Backend(self)
         server_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket1.bind(("0.0.0.0",8833))
-        server_socket1.listen()
-        print("socket1 is up and ready!")
+        # server_socket1.bind(("0.0.0.0",8833))
+        # server_socket1.listen()
+        self.array_of_sockets = array_of_sockets
+        # print("socket1 is up and ready!")
         (self.client_socket1,self.client_address1) = server_socket1.accept()
-        print("first client connected")
+        # print("first client connected")
         (self.client_socket2,self.client_address2) = server_socket1.accept()
-        print("secound client connected")
+        # print("secound client connected")
         self.question_and_ans = self.backend.next_question()
-
+        counter = 1
+        for x in self.array_of_sockets:
+                client_room = x
+                client_room.send("connected to server".encode())
+                print("client " + counter + " connected")
+                counter += 1
 
 
     
         
     def main(self):
         while True:
+            flag = 0
+            checks = []
             print("while")
-            check1 = self.client_socket1.recv(1024).decode() 
-            check2 = self.client_socket2.recv(1024).decode() 
-            print(check1)
-            print(check2)
-            if check1 == "didnt got question":
-                print("1")
-                self.sendQuestion() 
-            if check2 == "didnt got question":
-                print("2")       
-                self.sendQuestion()      
-            
-            if check1[:15] == "selected answer" and check2[:15] == "selected answer":  
-                print("4")
-                self.waitForAnswers(check1,check2)
-            if check1[:13] == "next question" and  check2[:13] == "next question":
-                print("5")
-                self.question_and_ans = self.backend.next_question()
-                self.sendQuestion()
+            for client in self.array_of_sockets:
+                checks.append(client.recv(1024).decode())
+            # check1 = self.client_socket1.recv(1024).decode() 
+            # check2 = self.client_socket2.recv(1024).decode() 
+            print(checks)
+            # print(check2)
+            for x in checks:
+                check = x
+                                
+                if check[:15] != "selected answer":  
+                    flag = 1
+            if flag == 0:
+               self.waitForAnswers(checks)
+            else:   
+                 flag = 0
+                 for x in checks:
+                     check = x   
+                    
+                     if check[:13] != "next question":
+                            flag = 1
+                     if flag == 0:
+                        self.question_and_ans = self.backend.next_question()
+                        self.sendQuestion()
             
                  
             
@@ -54,31 +67,42 @@ class Server():
         
         
     def sendQuestion(self):
-       
-        self.client_socket1.send("question".encode() + str(self.question_and_ans).encode())
-        self.client_socket2.send("question".encode() + str(self.question_and_ans).encode())
-        print("question sent")
+       counter = 1
+       for client in self.array_of_sockets:
+                client.send("question".encode() + str(self.question_and_ans).encode())
+                print("question sent to client " + counter)
+                counter += 1
+       self.main()        
+    #    self.client_socket1.send("question".encode() + str(self.question_and_ans).encode())
+    #    self.client_socket2.send("question".encode() + str(self.question_and_ans).encode())
+    #    print("question sent")
         
-        self.main()
         
             
             
-    def waitForAnswers(self,answer1,answer2):
-       
-            answer1 = self.reciveTheFullServer_sent(15,answer1)
+    def waitForAnswers(self,array_of_answers):
+              for answer in array_of_answers:
+                answer = self.reciveTheFullServer_sent(15,answer)
+                
+            # answer1 = self.reciveTheFullServer_sent(15,answer1)
            
-            answer2 = self.reciveTheFullServer_sent(15,answer2)
-            
-            self.backend.check_answer(0,answer1)
-            self.backend.check_answer(1,answer2)        
-            self.infoForClients()
+            # answer2 = self.reciveTheFullServer_sent(15,answer2)
+              self.backend.check_answer(array_of_answers)
+            #   self.backend.check_answer(0,answer1)
+            #   self.backend.check_answer(1,answer2)        
+              self.infoForClients()
                             
     def infoForClients(self):
-        player1_score = self.backend.get_score(0)
-        player2_score = self.backend.get_score(1)
-        
-        self.client_socket1.send("info".encode() + "a".encode() + str(player1_score).encode() + "b".encode() + str(player2_score).encode())
-        self.client_socket2.send("info".encode() + "a".encode() + str(player2_score).encode() + "b".encode() + str(player1_score).encode())
+        players_score = self.backend.get_score()
+        # player1_score = self.backend.get_score(0)
+        # player2_score = self.backend.get_score(1)
+        counter = 0
+        for x in players_score:
+            my_score = players_score[counter]
+            self.array_of_sockets[counter].send("info".encode() + str(my_score).encode() + "o".encode() + str(players_score).encode())
+            counter += 1
+            # self.client_socket1.send("info".encode() + "a".encode() + str(player1_score).encode() + "b".encode() + str(player2_score).encode())
+            # self.client_socket2.send("info".encode() + "a".encode() + str(player2_score).encode() + "b".encode() + str(player1_score).encode())
         
        
         
