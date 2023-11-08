68c75e87c9b4127c0a551ac49437d010caaeb9c5
jony <yhudashwartz2004@gmail.com>
2023-11-08 16:46:36 +0200

    working version

--- backend.py
+++ backend.py
@@ -1,10 +1,9 @@
 import question_data
-import random
 
 class Backend():
-    def __init__(self, server):
+    def __init__(self, server, player_names):
         self.server = server
-        self.player_names = ""
+        self.player_names = player_names
         self.turn_counter = 0
         self.scores = []  
         self.initiate_score_list()
@@ -36,17 +35,6 @@ class Backend():
         self.server.game_over(winners)
         
     def initiate_score_list(self):
-        print(self.player_names)
         for player in self.player_names:
             self.scores.append(0)
-    
-    def generate_random_room_id(self, array):
-        while True:
-            new_id = random.randint(10000, 99999)
-            if new_id not in array:
-                return new_id
-            
-    def get_list_of_names(self, names):
-        self.player_names = names
-            
         
